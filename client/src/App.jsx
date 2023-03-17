2944308f33db91606f02a1c5cb3bd00ff8cb9dc6
John Allan Hinds <jabistro@gmail.com>
2023-03-17 19:03:35 -0400

    new logo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-import { logo } from "./assets";
+import { aicamera } from "./assets";
 import { Home, CreatePost } from "./pages";
 
 const App = () => (
   <BrowserRouter>
     <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
       <Link to="/">
-        <img src={logo} alt="logo" className="w-28 object-contain" />
+        <img src={aicamera} alt="aicamera" className="w-28 object-contain" />
+        IntelliPics
       </Link>
 
       <Link
--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,5 +1,6 @@
 import download from "./download.png";
 import logo from "./logo.svg";
 import preview from "./preview.png";
+import aicamera from "./aicamera.png";
 
-export { download, logo, preview };
+export { download, logo, preview, aicamera };
